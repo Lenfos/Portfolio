06b658cb8d2586a28481476c5ebf93b253a45128
Lenfos <pierre7159@gmail.com>
2024-10-06 11:30:51 -0400

    Test burger menu

--- TS/Header.ts
+++ TS/Header.ts
@@ -6,10 +6,19 @@ class Header{
     constructor(){
         this.link = ["index", "formation", "projects", "contact"];
         this.header = document.getElementsByTagName("header")[0] as HTMLDivElement;
-        this.initialize();
+        if (screen.width <= 480){
+            this.initializeMobile();
+            document.getElementById("menu-burger").addEventListener("click", this.toggleClass.bind(this));
+            document.getElementById("menu-item").addEventListener("click", () => {
+                this.toggleClass();
+            });
+        }
+        else {
+            this.initializeDesktop();
+        }
     }
 
-    private initialize(){
+    private initializeDesktop(){
         let div = document.createElement("div");
         let nav = document.createElement("nav");
         let ul = document.createElement("ul");
@@ -34,6 +43,51 @@ class Header{
         img.src = "../assets/Logo.svg"
         this.header.appendChild(img);
         this.header.appendChild(div);
-
     }
+
+    private initializeMobile(){
+        let img = document.createElement("img");
+        img.id = "logo"
+        img.alt = "Pierre Vanhove's Logo"
+        img.src = "../assets/Logo.svg"
+
+        let div = document.createElement("div");
+        div.id = "globalDivMenu"
+
+        let menuBurger = document.createElement("div");
+        menuBurger.id = "menu-burger";
+        menuBurger.innerText = "☰";
+
+        let menuItems = document.createElement("div");
+        menuItems.id = "menu-item";
+
+        this.link.forEach((elem: string) => {
+            let tempDiv = document.createElement("div");
+            tempDiv.innerText = elem == "index" ? "Home" : elem.charAt(0).toUpperCase() + elem.slice(1);
+
+            tempDiv.addEventListener("click", () => {
+                location.href = elem + ".html";
+            });
+
+            menuItems.append(tempDiv);
+        })
+
+        div.appendChild(menuBurger);
+        div.appendChild(menuItems);
+
+        this.header.appendChild(img);
+        this.header.appendChild(div);
+    }
+
+
+    private toggleClass(){
+        console.log("ok");
+        let menuBurger = document.getElementById("menu-burger") as HTMLDivElement;
+
+        menuBurger.classList.toggle("fs");
+        document.getElementById("menu-item").classList.toggle("fs");
+
+        menuBurger.innerText = menuBurger.innerText == "☰" ? "✕" : "☰";
+    }
+
 }
--- js/Header.js
+++ js/Header.js
@@ -2,9 +2,18 @@ class Header {
     constructor() {
         this.link = ["index", "formation", "projects", "contact"];
         this.header = document.getElementsByTagName("header")[0];
-        this.initialize();
+        if (screen.width <= 480) {
+            this.initializeMobile();
+            document.getElementById("menu-burger").addEventListener("click", this.toggleClass.bind(this));
+            document.getElementById("menu-item").addEventListener("click", () => {
+                this.toggleClass();
+            });
+        }
+        else {
+            this.initializeDesktop();
+        }
     }
-    initialize() {
+    initializeDesktop() {
         let div = document.createElement("div");
         let nav = document.createElement("nav");
         let ul = document.createElement("ul");
@@ -27,6 +36,38 @@ class Header {
         img.src = "../assets/Logo.svg";
         this.header.appendChild(img);
         this.header.appendChild(div);
+    }
+    initializeMobile() {
+        let img = document.createElement("img");
+        img.id = "logo";
+        img.alt = "Pierre Vanhove's Logo";
+        img.src = "../assets/Logo.svg";
+        let div = document.createElement("div");
+        div.id = "globalDivMenu";
+        let menuBurger = document.createElement("div");
+        menuBurger.id = "menu-burger";
+        menuBurger.innerText = "☰";
+        let menuItems = document.createElement("div");
+        menuItems.id = "menu-item";
+        this.link.forEach((elem) => {
+            let tempDiv = document.createElement("div");
+            tempDiv.innerText = elem == "index" ? "Home" : elem.charAt(0).toUpperCase() + elem.slice(1);
+            tempDiv.addEventListener("click", () => {
+                location.href = elem + ".html";
+            });
+            menuItems.append(tempDiv);
+        });
+        div.appendChild(menuBurger);
+        div.appendChild(menuItems);
+        this.header.appendChild(img);
+        this.header.appendChild(div);
+    }
+    toggleClass() {
+        console.log("ok");
+        let menuBurger = document.getElementById("menu-burger");
+        menuBurger.classList.toggle("fs");
+        document.getElementById("menu-item").classList.toggle("fs");
+        menuBurger.innerText = menuBurger.innerText == "☰" ? "✕" : "☰";
     }
 }
 //# sourceMappingURL=Header.js.map
