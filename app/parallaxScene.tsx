03af6d2ebf30642ba323e9ee4e062387e1890522
Pierre Vanhove <pierre.vanhove@iut-dijon.u-bourgogne.fr>
2025-07-31 23:30:24 +0200

    Add title and resolve parallax bug

--- app/parallaxScene.tsx
+++ app/parallaxScene.tsx
@@ -4,38 +4,38 @@ import {Parallax, ParallaxLayer} from "@react-spring/parallax";
 
 export default function ParallaxScene() {
     return(
-        <Parallax pages={2} style={{top:'0', left: '0', height:'100vh'}} className="animation">
+        <Parallax pages={2} style={{top:'0', left: '0'}} className="animation">
             <ParallaxLayer offset={0} speed={0.1}>
                 <div id="background" className="animation_layer parallax bg-[url('./image/Background.svg')] bottom-0 top-0" style={{backgroundSize: 'cover'}}></div>
             </ParallaxLayer>
             <ParallaxLayer offset={0} speed={0.15}>
                 <div id="stars" className="animation_layer parallax bg-[url('./image/Stars.svg')] bottom-0 left-30" style={{width:'60em', top: '-40em'}}></div>
             </ParallaxLayer>
-            <ParallaxLayer offset={0} speed={0.3}>
+            <ParallaxLayer offset={0} speed={0.16}>
                 <div id="sun" className="animation_layer parallax bg-[url('./image/Sun.svg')] right-70 top-70" style={{backgroundRepeat: 'no-repeat', height:'300px'}}></div>
             </ParallaxLayer>
-            <ParallaxLayer offset={0} speed={0.4}>
+            <ParallaxLayer offset={0} speed={0.2}>
                 <div id="clouds" className="animation_layer parallax bg-[url('./image/clouds.svg')] bottom-0" style={{top: '-22em'}}></div>
             </ParallaxLayer>
-            <ParallaxLayer offset={0} speed={0.4}>
+            <ParallaxLayer offset={0} speed={0.2}>
                 <div id="mountainBack" className="animation_layer parallax bg-[url('./image/MoutainBack.svg')] bottom-0" style={{top:'-8em'}}></div>
             </ParallaxLayer>
-            <ParallaxLayer offset={0} speed={0.4}>
-                <div id="Name" className="animation_layer parallax bottom-0" style={{top: '20em'}}>
-                    <h1 className={"font-gila text-4xl text-[color:var(--name-foreground)]"} style={{textShadow: 'var(--name-shadows)'}}>PIERRE VANHOVE</h1>
+            <ParallaxLayer offset={0} speed={0.1}>
+                <div id="Name" className="animation_layer parallax bottom-0 flex justify-center" style={{top: '15em'}}>
+                    <h1 className={"font-gila text-6xl text-[color:var(--name-foreground)] "} style={{textShadow: 'var(--name-shadows)'}}>PIERRE VANHOVE</h1>
                 </div>
             </ParallaxLayer>
-            <ParallaxLayer offset={0} speed={0.5}>
+            <ParallaxLayer offset={0} speed={0.3}>
                 <div id="moutainMiddle" className="animation_layer parallax bg-[url('./image/MoutainMiddle.svg')] bottom-0" style={{top: '-8em'}}></div>
             </ParallaxLayer>
-            <ParallaxLayer offset={0.99} speed={0.3}>
-                <div className="w-full h-screen bg-[#5F0130] bottom-0" style={{top:'0'}}></div>
-            </ParallaxLayer>
-            <ParallaxLayer offset={0} speed={0.6}>
+            <ParallaxLayer offset={0} speed={0.4}>
                 <div id="moutainFront" className="animation_layer parallax bg-[url('./image/MountainFront.svg')] bottom-0" style={{top:'-10em', backgroundSize: 'contain'}}></div>
             </ParallaxLayer>
-
-
+            <ParallaxLayer offset={1} speed={0.3}>
+                <div className="w-full h-screen bg-[#5D002E] bottom-0 flex justify-center items-center" style={{top:'0'}}>
+                    <h2 className={"font-gila text-4xl text-white"}>About Me</h2>
+                </div>
+            </ParallaxLayer>
         </Parallax>
     )
 }
